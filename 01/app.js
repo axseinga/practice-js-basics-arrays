82ceebaee309f28db72e4ecb4ce6654c10b432b6
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-06-16 09:59:42 +0100

    01 solved

---
+++ 01/app.js
@@ -0,0 +1,14 @@
+const users = [
+  "Agnieszka Urbanowicz",
+  "Anna Kowalska",
+  "Jan Kowalski",
+  "Adam Nowak",
+  "Juliusz Slowacki",
+  "Adam Mickiewicz",
+];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
+console.log(users.length);
