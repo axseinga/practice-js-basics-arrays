a740fa7839bcbf5809a005d1d0d40cb5b980dbd3
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-06-16 12:03:25 +0100

    05 solved

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+/*const oddNumbers = numbers.filter((n) => n % 2 == 0);
+console.log(oddNumbers);
+
+const sum = oddNumbers.reduce((acc, el) => acc + el);
+console.log(sum);*/
+
+const sum = numbers.filter((n) => n % 2 == 0).reduce((acc, el) => acc + el);
+console.log(sum);
