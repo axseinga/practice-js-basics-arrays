50c796090410ad0d5580f51b67a3ebd68cf807f6
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-06-16 13:09:27 +0100

    03 solved

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,13 @@
 const n = 24;
 const oddNumbers = [];
+
+const getOddNumbers = function (n) {
+  for (let i = 1; i <= n; i++) {
+    if (!(i % 2 === 0)) {
+      oddNumbers.push(i);
+    }
+  }
+  return oddNumbers;
+};
+
+console.log(getOddNumbers(n));
